10d646b8ddb449461b5077f6ee6bdc08c415a0d9
Ben Flynn <flynn0087@gmail.com>
2020-10-17 14:51:01 -0500

    Changed arrays to strings

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-let lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let intiger = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let special = [' ', '!', '\u0021', '#', '$', '%', '&', '\u0027', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '\u005B', '\u005C', '\u005D', '^', '_', '`', '{', '|', '}', '~'];
+let lower ="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+let upper ="A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+let intiger ="0,1,2,3,4,5,6,7,8,9";
+let special =" !#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\""
 
 // Write password to the #password input
 function writePassword() {
