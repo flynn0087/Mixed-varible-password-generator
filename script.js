a75621120f30c775cb0613732e7c083c7cd4c89f
Ben Flynn <flynn0087@gmail.com>
2020-10-17 17:37:22 -0500

    function details the length of PW

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ let special =" !#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\""
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+function generatePassword() {
+  let pwLength = prompt("Between 8 and 128; how many characters would you like to use?");
+    if (pwLength <= 128 && pwLength >= 8);
+      else { alert("Password needs to be between 8 and 128 characters")
+      return generatePassword()
+      }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
