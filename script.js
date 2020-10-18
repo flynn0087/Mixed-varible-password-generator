1a37f69323a189d750ba8c383a0f424b233e6aad
Ben Flynn <flynn0087@gmail.com>
2020-10-17 19:50:41 -0500

    password fuction added, results generated

--- script.js
+++ script.js
@@ -16,15 +16,33 @@ function generatePassword() {
   let pwLength = prompt("Between 8 and 128; how many characters would you like to use?");
     if (pwLength <= 128 && pwLength >= 8);
       else { alert("Password needs to be between 8 and 128 characters")
-      return generatePassword()
+        return generatePassword()
       }
 
          
   let lowerCase = confirm("Do you want lower case letters?");
   let upperCase = confirm("Do you want upper case letters?"); 
-  let intigerCase = confirm("Do you want numbers letters?");
-  let specialCase = confirm("Do you want special characters letters?");
+  let intigerCase = confirm("Do you want numbers?");
+  let specialCase = confirm("Do you want special characters?");
   let char = "";
+
+  if (lowerCase === false && upperCase === false && intigerCase === false && specialCase === false) {alert("At least one character set must be chosen"); return generatePassword()}     
+  if (lowerCase === true) {char = char + lower};
+  if (upperCase === true) {char = char + upper};
+  if (intigerCase === true) {char = char + intiger};
+  if (specialCase === true) {char = char + special};
+
+  let result = password (pwLength, char);
+  return result;
+}
+
+function password(pwLength, char) {
+  let pwd = "";
+  for (let i = 0; i < pwLength; i++) {
+    pwd += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return pwd;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
